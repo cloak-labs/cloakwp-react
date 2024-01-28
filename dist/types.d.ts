d86650c3ddbcbf5d5a1e1cd4b4250344259d0cc4
Kaelan Smith <kaelancsmith@gmail.com>
2024-01-27 23:06:04 -0800

    change workspace:* dep versions to hard numbers

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,5 +1,5 @@
 /**
- * Wrap all generic WP `render-blocks` types to make them React-specific.
+ * Wrap all generic WP `@kaelan/render-blocks` types to make them React-specific.
  * Doing so prevents users from having to mess with type parameters all over the place.
  */
 /// <reference types="react" />
--- dist/types.js
+++ dist/types.js
@@ -1,6 +1,6 @@
 "use strict";
 /**
- * Wrap all generic WP `render-blocks` types to make them React-specific.
+ * Wrap all generic WP `@kaelan/render-blocks` types to make them React-specific.
  * Doing so prevents users from having to mess with type parameters all over the place.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
